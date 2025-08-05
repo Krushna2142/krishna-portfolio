afb6f4db33c915b10b531960d183bb30bb51406c
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-08-05 14:54:04 +0530

    Initalcommit

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 
 router.post('/', createMessage); // Public
 router.get('/', verifyAdmin, getMessages); // Admin only
-router.delete('/:id', protect, deleteMessage); // Admin only
+router.delete('/:id', verifyAdmin, deleteMessage); // Admin only
 
 export default router;
