281c3cc261e7cff18b6aee6115a54123aaaa8d82
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-08-05 14:52:24 +0530

    Initalcommit

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -2,12 +2,12 @@
 
 import express from 'express';
 import { createMessage, getMessages, deleteMessage } from '../controllers/messageController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { verifyAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', createMessage); // Public
-router.get('/', protect, getMessages); // Admin only
+router.get('/', verifyAdmin, getMessages); // Admin only
 router.delete('/:id', protect, deleteMessage); // Admin only
 
 export default router;
