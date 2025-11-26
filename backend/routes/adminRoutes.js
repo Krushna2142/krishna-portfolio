ce6b3e263209a801c5fecac7d744ca87fe3d4bc6
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-11-26 16:12:31 +0530

    intialcommit

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,9 @@
-import express from "express";
-import { registerAdmin, loginAdmin } from "../controllers/adminController.js";
+const express = require("express");
+const { registerAdmin, loginAdmin } = require("../controllers/adminController");
 
 const router = express.Router();
 
 router.post("/register", registerAdmin);   // only use once
 router.post("/login", loginAdmin);
 
-export default router;
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import adminRoutes from "./routes/adminRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -10,9 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
-app.use("/api/messages", require("./routes/contactRoutes"));
+app.use("/api/admin", adminRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
