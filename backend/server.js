42d7ecdeb75ae8bc0d0d680e5212d7adaeb98a1f
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-12-01 00:04:12 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -53,5 +53,5 @@ app.get("/", (req, res) => {
   res.send("Backend Running...");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.SERVER_PORT || 5000;
+app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`));
